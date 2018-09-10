9488712de72f2d07115d8b92bbdb1023291d6907
Stavroula Guerrero <4stacyg@optonline.net>
2018-09-10 17:36:08 +0000

    Refactor

--- lib/greek_islands/cli.rb
+++ lib/greek_islands/cli.rb
@@ -9,19 +9,21 @@ class GreekIslands::CLI
   def list_islands
      puts "20 Best Greek Islands:"
      @islands = GreekIslands::Islands.all
-  end
+     @islands.each.with_index(1) do |island, i|
+       puts "#{i}. #{island.name} - #{island.stay}"
+     end
+   end
   
   def menu
     input = nil
     while input != "exit"
-    puts "Enter the number of the island you'd like more info on or type list to see the islands again or type exit:"
-    input = gets.strip.downcase
-    case input
-    when "1"
-      puts "More info on island 1..."
-      when "2" 
-        puts "More info on island 2..."
-      when "list"
+      puts "Enter the number of the island you'd like more info on or type list to see the islands again or type exit:"
+      input = gets.strip.downcase
+      
+      if input.to_i > 0
+        the_island = @islands[input.to_i-1]
+        puts "#{the_island.name} - #{the_island.stay}"
+      elsif input == "list"
         list_islands
       else
         puts "Please type list or exit."
@@ -29,7 +31,6 @@ class GreekIslands::CLI
     end
   end
 
-  
   def goodbye
     puts "Thank you for visiting the Greek Islands!!"
   end
--- lib/greek_islands/islands.rb
+++ lib/greek_islands/islands.rb
@@ -1,10 +1,18 @@
 class GreekIslands::Islands 
+  attr_accessor :name, :stay
   
   def self.all
-    puts <<~DOC
-    1. Hydra - Places to stay in Hydra.
-    2. Corfu - Places to stay in Corfu.
-    DOC
+    
+    island_1 = self.new 
+    island_1.name = "Hydra"
+    island_1.stay = "Orloff Boutique Hotel"
+    
+    island_2 = self.new 
+    island_2.name = "Corfu"
+    island_2.stay = "Rou Estate"
+    
+    [island_1, island_2]
   end
+  
 end
     
