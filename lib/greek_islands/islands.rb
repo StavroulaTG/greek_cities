3e8131b6c1301b55f88f58fe2e0f43ecb672f786
Stavroula Guerrero <4stacyg@optonline.net>
2018-09-17 16:41:57 +0000

    Refactor

--- lib/greek_islands/islands.rb
+++ lib/greek_islands/islands.rb
@@ -1,5 +1,5 @@
 class GreekIslands::Islands 
-  attr_accessor :name, :stay, :url 
+  attr_accessor :name, :stay
   
   def self.all 
     self.scrape_islands
@@ -9,19 +9,22 @@ class GreekIslands::Islands
     islands = []
     
     islands << self.scrape_cntraveller
-    
+   
     islands
+    
   end
   
   def self.scrape_cntraveller
+    
     doc = Nokogiri::HTML(open("https://www.cntraveller.com/gallery/best-greek-islands-beaches"))
     
     island = self.new
-    island.name = doc.search("div.c-figure__title").text
-    island.stay = doc.search("strong.bb-strong").text
-    island.url = "https://github.com/StavroulaTG/greek_islands"
-    binding.pry 
+    
+    island.name = doc.css("div.c-figure__title").text
+    island.stay = doc.css("strong.bb-strong css selector").text
+
     island
   end
+
 end
     
